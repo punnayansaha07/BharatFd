4f4c7a76d0287dfda463d3c66e25fd5e48044605
punnayansaha07 <punnayansaha18@gmail.com>
2025-02-01 02:12:20 +0530

    search and filter added for admin

--- faq/admin.py
+++ faq/admin.py
@@ -1,4 +1,31 @@
 from django.contrib import admin
 from .models import FAQ
+from django.utils.translation import gettext_lazy as _
 
-admin.site.register(FAQ)
+class FAQAdmin(admin.ModelAdmin):
+    list_display = ('question', 'answer', 'get_translations', 'id', 'created_at', 'updated_at')
+    search_fields = ('question',)
+    list_filter = ('created_at', 'updated_at')
+    
+    fieldsets = (
+        (None, {
+            'fields': ('question', 'answer')
+        }),
+        (_('Translations'), {
+            'fields': ('get_translations',)
+        }),
+    )
+    
+    def get_translations(self, obj):
+        return ', '.join([
+            f"{lang.upper()}: {obj.get_translated_question(lang)}" 
+            for lang in ['hi', 'bn', 'sw', 'en']
+        ])
+    get_translations.short_description = _('Translations')
+
+    formfield_overrides = {
+        'answer': {'widget': admin.widgets.AdminTextareaWidget},
+    }
+
+admin.site.register(FAQ, FAQAdmin)
+

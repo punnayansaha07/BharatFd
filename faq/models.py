86c900387e8453d4b5c3b7cdd2d0d95c006d1933
punnayansaha07 <punnayansaha18@gmail.com>
2025-02-01 01:04:21 +0530

    Multilingual FAQ model

--- faq/models.py
+++ faq/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from ckeditor.fields import RichTextField
+from googletrans import Translator
 
-# Create your models here.
+translator = Translator()
+
+class FAQ(models.Model):
+    question = models.TextField()
+    answer = RichTextField()
+    question_hi = models.TextField(blank=True, null=True)
+    question_bn = models.TextField(blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        if not self.question_hi:
+            self.question_hi = translator.translate(self.question, src='en', dest='hi').text
+        if not self.question_bn:
+            self.question_bn = translator.translate(self.question, src='en', dest='bn').text
+        super().save(*args, **kwargs)
+
+    def get_translated_question(self, lang):
+        return getattr(self, f'question_{lang}', self.question)
+    
+    def __str__(self):
+        return self.question

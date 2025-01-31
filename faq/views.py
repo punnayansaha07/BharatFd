c748aa8b919ae9170f16960116a535137e470779
punnayansaha07 <punnayansaha18@gmail.com>
2025-02-01 01:13:08 +0530

    API Implemented for multilingual FAQs

--- faq/views.py
+++ faq/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.core.cache import cache
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import FAQ
+from .serializers import FAQSerializer
 
-# Create your views here.
+class FAQListView(APIView):
+    def get(self, request):
+        lang = request.GET.get('lang', 'en')
+        cache_key = f'faqs_{lang}'
+        cached_data = cache.get(cache_key)
+
+        if cached_data:
+            return Response(cached_data)
+
+        faqs = FAQ.objects.all()
+        for faq in faqs:
+            faq.question = faq.get_translated_question(lang)
+
+        serializer = FAQSerializer(faqs, many=True)
+        cache.set(cache_key, serializer.data, timeout=60 * 60)
+
+        return Response(serializer.data)

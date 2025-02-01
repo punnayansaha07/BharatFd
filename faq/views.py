a3b74d56bdd8f86e92aadc4700b9af13aac3da0f
punnayansaha07 <punnayansaha18@gmail.com>
2025-02-01 06:34:33 +0530

    pagination added

--- faq/views.py
+++ faq/views.py
@@ -1,10 +1,10 @@
-from django.core.cache import cache
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.pagination import PageNumberPagination
 from .models import FAQ
 from .serializers import FAQSerializer
 from rest_framework import status
+from django.core.cache import cache
 
 class FAQListView(APIView):
     def get(self, request):
@@ -18,15 +18,15 @@ class FAQListView(APIView):
         paginator = PageNumberPagination()
         paginator.page_size = 20
 
-        faqs = FAQ.objects.all()
+        faqs = FAQ.objects.all().order_by('created_at') 
 
         for faq in faqs:
             faq.question = faq.get_translated_question(lang)
+            faq.answer = faq.get_translated_answer(lang)
 
         result_page = paginator.paginate_queryset(faqs, request)
 
         serializer = FAQSerializer(result_page, many=True)
-
         cache.set(cache_key, serializer.data, timeout=60 * 60)
 
         return paginator.get_paginated_response(serializer.data)

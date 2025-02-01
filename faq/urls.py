f340a922557a08ae4d9ff56ebfcac787979928fa
punnayansaha07 <punnayansaha18@gmail.com>
2025-02-01 06:32:01 +0530

    urls fixed

--- faq/urls.py
+++ faq/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import FAQListView
 
 urlpatterns = [
-    path('api/v1/faqs/', FAQListView.as_view(), name='faq-list-v1'),
+    path('faqs/', FAQListView.as_view(), name='faq-list'),
 ]
